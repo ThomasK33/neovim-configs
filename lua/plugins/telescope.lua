f1e9175cab9bbf8d23323eb96249d17b160896a4
Thomas Kosiewski <thomas.kosiewski@loft.sh>
2024-02-16 14:37:20 +0100

    Added telescope-live-grep-args.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,61 @@
 return {
-  "telescope.nvim",
-  opts = function()
-    local actions = require("telescope.actions")
+  {
+    "telescope.nvim",
+    opts = function()
+      local actions = require("telescope.actions")
 
-    local select_one_or_multi = function(prompt_bufnr)
-      local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-      local multi = picker:get_multi_selection()
-      if not vim.tbl_isempty(multi) then
-        require("telescope.actions").close(prompt_bufnr)
-        for _, j in pairs(multi) do
-          if j.path ~= nil then
-            vim.cmd(string.format("%s %s", "edit", j.path))
+      local select_one_or_multi = function(prompt_bufnr)
+        local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if not vim.tbl_isempty(multi) then
+          require("telescope.actions").close(prompt_bufnr)
+          for _, j in pairs(multi) do
+            if j.path ~= nil then
+              vim.cmd(string.format("%s %s", "edit", j.path))
+            end
           end
+        else
+          require("telescope.actions").select_default(prompt_bufnr)
         end
-      else
-        require("telescope.actions").select_default(prompt_bufnr)
       end
-    end
 
-    return {
-      defaults = {
-        mappings = {
-          i = {
-            ["«"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<CR>"] = select_one_or_multi,
-          },
-          n = {
-            ["«"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<CR>"] = select_one_or_multi,
+      return {
+        defaults = {
+          mappings = {
+            i = {
+              ["«"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<CR>"] = select_one_or_multi,
+            },
+            n = {
+              ["«"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<CR>"] = select_one_or_multi,
+            },
           },
         },
+      }
+    end,
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
       },
-    }
-  end,
+    },
+    keys = {
+      {
+        "<leader>fg",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Grep Args",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end,
+  },
 }
