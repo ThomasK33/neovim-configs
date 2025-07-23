9b4e175bfebb6ef681f33bd0b9fa95f7f9cd2b2d
Thomas Kosiewski <tk@coder.com>
2025-07-23 11:36:53 +0200

    Add claude code extension

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    dir = "/Users/thomask33/GitHub/claudecode.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      -- Optional configuration
+      -- log_level = "debug",
+      -- terminal_cmd = "claude --debug",
+      -- terminal = {
+      --   provider = "native",
+      -- },
+    },
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      -- Add file from nvim-tree or neo-tree
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree" },
+      },
+    },
+    dev = true,
+  },
+}
--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,3 +1,26 @@
+local pick = function()
+  local refactoring = require("refactoring")
+  if LazyVim.pick.picker.name == "telescope" then
+    return require("telescope").extensions.refactoring.refactors()
+  elseif LazyVim.pick.picker.name == "fzf" then
+    local fzf_lua = require("fzf-lua")
+    local results = refactoring.get_refactors()
+
+    local opts = {
+      fzf_opts = {},
+      fzf_colors = true,
+      actions = {
+        ["default"] = function(selected)
+          refactoring.refactor(selected[1])
+        end,
+      },
+    }
+    fzf_lua.fzf_exec(results, opts)
+  else
+    refactoring.select_refactor()
+  end
+end
+
 return {
   "ThePrimeagen/refactoring.nvim",
   opts = {
@@ -5,6 +28,91 @@ return {
       go = {
         'fmt.Printf("%s %%v\\n", %s)',
       },
+    },
+  },
+  keys = {
+    { "<leader>r", "", desc = "+refactor", mode = { "n", "x" } },
+    {
+      "<leader>rs",
+      pick,
+      mode = { "n", "x", "v" },
+      desc = "Refactor",
+    },
+    {
+      "<leader>ri",
+      function()
+        return require("refactoring").refactor("Inline Variable")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Inline Variable",
+      expr = true,
+    },
+    {
+      "<leader>rb",
+      function()
+        return require("refactoring").refactor("Extract Block")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Extract Block",
+      expr = true,
+    },
+    {
+      "<leader>rf",
+      function()
+        return require("refactoring").refactor("Extract Block To File")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Extract Block To File",
+      expr = true,
+    },
+    {
+      "<leader>rP",
+      function()
+        require("refactoring").debug.printf({ below = false })
+      end,
+      desc = "Debug Print",
+    },
+    {
+      "<leader>rp",
+      function()
+        require("refactoring").debug.print_var({ normal = true })
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Debug Print Variable",
+    },
+    {
+      "<leader>rc",
+      function()
+        require("refactoring").debug.cleanup({})
+      end,
+      desc = "Debug Cleanup",
+    },
+    {
+      "<leader>rf",
+      function()
+        return require("refactoring").refactor("Extract Function")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Extract Function",
+      expr = true,
+    },
+    {
+      "<leader>rF",
+      function()
+        return require("refactoring").refactor("Extract Function To File")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Extract Function To File",
+      expr = true,
+    },
+    {
+      "<leader>rx",
+      function()
+        return require("refactoring").refactor("Extract Variable")
+      end,
+      mode = { "n", "x", "v" },
+      desc = "Extract Variable",
+      expr = true,
     },
   },
 }
